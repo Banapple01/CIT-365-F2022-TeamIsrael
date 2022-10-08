84b31fe708679ba965bf7bf04a8595bbbda667dc
sbunkerchris <50494698+sbunkerchris@users.noreply.github.com>
2022-10-07 20:16:20 -0600

    SearchQuotes.cs Progress. *Work on FilterList()*

--- MegaDesk-Anderson/MegaDesk-Anderson/SearchQuotes.Designer.cs
+++ MegaDesk-Anderson/MegaDesk-Anderson/SearchQuotes.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.BackToHome = new System.Windows.Forms.Button();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.MatBox = new System.Windows.Forms.ComboBox();
+            this.MatLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // BackToHome
@@ -37,28 +40,79 @@
             this.BackToHome.FlatAppearance.BorderSize = 2;
             this.BackToHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BackToHome.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BackToHome.Location = new System.Drawing.Point(287, 305);
+            this.BackToHome.Location = new System.Drawing.Point(35, 318);
+            this.BackToHome.Margin = new System.Windows.Forms.Padding(4);
             this.BackToHome.Name = "BackToHome";
-            this.BackToHome.Size = new System.Drawing.Size(80, 80);
+            this.BackToHome.Size = new System.Drawing.Size(107, 98);
             this.BackToHome.TabIndex = 0;
             this.BackToHome.Text = "Back";
             this.BackToHome.UseVisualStyleBackColor = true;
             this.BackToHome.Click += new System.EventHandler(this.BackButton_Click);
             // 
+            // SearchButton
+            // 
+            this.SearchButton.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.SearchButton.FlatAppearance.BorderSize = 2;
+            this.SearchButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SearchButton.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchButton.Location = new System.Drawing.Point(194, 318);
+            this.SearchButton.Margin = new System.Windows.Forms.Padding(4);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(107, 98);
+            this.SearchButton.TabIndex = 1;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MatBox
+            // 
+            this.MatBox.FormattingEnabled = true;
+            this.MatBox.Items.AddRange(new object[] {
+            "Oak",
+            "Pine",
+            "Veneer",
+            "Laminate",
+            "Redwood"});
+            this.MatBox.Location = new System.Drawing.Point(169, 69);
+            this.MatBox.Name = "MatBox";
+            this.MatBox.Size = new System.Drawing.Size(132, 24);
+            this.MatBox.TabIndex = 2;
+            this.MatBox.Text = "Select Material";
+            // 
+            // MatLabel
+            // 
+            this.MatLabel.AutoSize = true;
+            this.MatLabel.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MatLabel.Location = new System.Drawing.Point(35, 65);
+            this.MatLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.MatLabel.Name = "MatLabel";
+            this.MatLabel.Size = new System.Drawing.Size(107, 26);
+            this.MatLabel.TabIndex = 24;
+            this.MatLabel.Text = "Material:";
+            // 
             // SearchQuotes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(338, 444);
+            this.Controls.Add(this.MatLabel);
+            this.Controls.Add(this.MatBox);
+            this.Controls.Add(this.SearchButton);
             this.Controls.Add(this.BackToHome);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "SearchQuotes";
             this.Text = "SearchQuotes";
+            this.Load += new System.EventHandler(this.SearchQuotes_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button BackToHome;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.ComboBox MatBox;
+        private System.Windows.Forms.Label MatLabel;
     }
 }
--- MegaDesk-Anderson/MegaDesk-Anderson/SearchQuotes.cs
+++ MegaDesk-Anderson/MegaDesk-Anderson/SearchQuotes.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace MegaDesk_Anderson
 {
@@ -17,11 +19,46 @@ namespace MegaDesk_Anderson
             InitializeComponent();
         }
 
+        //Read in json file to List of objects
+        private List<object> ReadFile(string filename)
+        {
+            var orderData= new List<object>();
+            File.WriteAllText(filename, JsonConvert.SerializeObject(orderData));
+            return orderData;
+        }
+        // ***** this function is where i need help !!!!!
+        //filters through orders returning a List<string> with only the material type
+        private List<object> FilterList(List<object> orders, string material)
+        {
+            /* trying to use a query to select and check if the Material matches the
+             * material the user selected. if it matches put it into the filterred */
+            List<object> filtered = from order in orders where /* select Material */ == material select /* ordered material */  ; 
+
+            return filtered;
+        }
         private void BackButton_Click(object sender, EventArgs e)
         {
             MainMenu viewMainMenu = (MainMenu)Tag;
             viewMainMenu.Show();
             Close();
+        }
+
+        private void SearchQuotes_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // set filename of the json file where all orders are saved
+            string filename = "jsconfig1.json";
+            List<object> Orders = ReadFile(filename);
+
+            string mat = MatBox.GetItemText(MatBox.SelectedItem);
+            /* filteredOrders holds all orders that match slected material
+             * this List<object> would need to be sent to another windows form that 
+             * will display the List<object> */
+            List<object> filteredOrders = FilterList(Orders, mat);
         }
     }
 }
