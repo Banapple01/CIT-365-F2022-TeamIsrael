6ac49b9210bde4716e43a72cd3278dee210415e4
angela.brunst <angela.hairstylist@gmail.com>
2022-11-05 18:39:12 -0600

    Updated details page & dropped database

---
+++ MegaDeskRazorPages/Migrations/20221106003120_UpdateDatabase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MegaDeskRazorPages.Migrations
+{
+    public partial class UpdateDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Material",
+                table: "Quote",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Material",
+                table: "Quote",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
