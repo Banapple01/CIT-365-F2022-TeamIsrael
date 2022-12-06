704d7cc6cfe412f93636fb3a9e1dcbc9492f0c27
KendraAnderson <kor04003@byui.edu>
2022-12-05 19:17:59 -0800

    partial model

--- SacramentPlanner/Models/Planner.cs
+++ SacramentPlanner/Models/Planner.cs
@@ -1,7 +1,42 @@
-﻿namespace SacramentPlanner.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SacramentPlanner.Models
 {
     public class Planner
     {
         public int Id { get; set; }
+
+        [Display(Name = "Planner Date")]
+        [DataType(DataType.Date)]
+        public DateTime PlannerDate { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        public string Conducting { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        public string Presiding { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string OpeningHymn { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string SacramentHymn { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string ClosingHymn { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        public string IntermediateHymn { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string OpeningPrayer { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string ClosingPrayer { get; set; }
     }
 }
